f9653d93ca364f00501eca46c7adad7017165857
Justin G. Cramer <jcramer@proshipinc.com>
2019-02-17 12:22:18 -0600

    Completed lesson 16.12 Promises part 2

---
+++ p5js/16_topics_of_js_es6-8/16.12_promises_part_2.js
@@ -0,0 +1,24 @@
+function setup() {
+     noCanvas();
+     //  setTimeout(sayHello, 1000);
+     delay(1000)
+          .then(() => createP('hello'))
+          .catch(err => console.error(err));
+     delay('bad')
+          .then(() => createP('hello'))
+          .catch(err => console.error(err));
+}
+
+function delay(time) {
+     return new Promise((resolve, reject) => {
+          if (isNaN(time)) {
+               reject(new Error('delay requires a valid number '));
+          }
+          setTimeout(resolve, time);
+     });
+     //  setTimeout(sayHello, time);
+}
+
+function sayHello() {
+     createP('hello');
+}
