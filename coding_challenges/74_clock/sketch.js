5aab3c8f6b07f124ec5e9eb4f190b976e4e091a6
Justin G. Cramer <jcramer@proshipinc.com>
2021-11-28 12:01:14 -0600

    Clean up

--- coding_challenges/74_clock/sketch.js
+++ coding_challenges/74_clock/sketch.js
@@ -6,11 +6,11 @@ function draw() {
      background(0);
 
      let hr = hour();
-     let mn = minuet();
+     let mn = minute();
      let sc = second();
 
      fill(255);
-     // noStroke();
+     noStroke();
      text("something", 200, 200);
      text(hr + ':' + mn + ':' + sc, 10, 200);
 
