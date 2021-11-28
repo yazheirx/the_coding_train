6ab6fe8a36115c05fa0caa6edd99b0f59437511c
Justin G. Cramer <jcramer@proshipinc.com>
2021-11-28 11:40:29 -0600

    Struggling with geting text to show

--- coding_challenges/74_clock/sketch.js
+++ coding_challenges/74_clock/sketch.js
@@ -4,4 +4,14 @@ function setup() {
 
 function draw() {
      background(0);
+
+     let hr = hour();
+     let mn = minuet();
+     let sc = second();
+
+     fill(255);
+     // noStroke();
+     text("something", 200, 200);
+     text(hr + ':' + mn + ':' + sc, 10, 200);
+
 }
