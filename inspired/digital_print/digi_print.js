168577afa18222ae7806c300c3d2dc8e464cea7b
Justin G. Cramer <jcramer@proshipinc.com>
2021-11-28 12:57:25 -0600

    Intital comments created

---
+++ inspired/digital_print/digi_print.js
@@ -0,0 +1,10 @@
+// Purpose: Pass an Input that will be rendered to a p5.js canvas in a dot matrix font
+
+function digi_print(input){
+
+}
+
+// this array will hold the mapping from the char to the pixles
+let digi_font = {
+
+};
